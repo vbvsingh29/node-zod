e08ce71fdc664904ed7b1cc8b751a190433689c0
vbvsingh29 <vbvsingh2905@gmail.com>
2024-02-19 09:37:52 +0530

    main server file

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import config  from 'config';
+import connect from './utlis/connect';
+import logger from './utlis/logger';
+import routes from './routes';
+const app = express();
+const port = config.get<number>('port');
+
+app.use(express.json());
+app.listen(1337,async()=>{
+    logger.info(`server is running at http://localhost:${port}`)
+
+    await connect();
+    routes(app);
+})
