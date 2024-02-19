443251b2147d0abbf6440410f2230d5eaa919973
vbvsingh29 <vbvsingh2905@gmail.com>
2024-02-19 09:46:36 +0530

    --routes + model for user registration--

---
+++ src/models/user.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+export interface UserInput {
+  email: string;
+  name: string;
+  password: string;
+}
+export interface UserDocuemnt extends UserInput, mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next: (err?: Error) => void) {
+  let user = this as UserDocuemnt;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<Boolean> {
+  const user = this as UserDocuemnt;
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+const UserModel = mongoose.model("User", userSchema);
+export default UserModel;
