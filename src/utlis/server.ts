08c5e5ff1c1abe6fe9f3160cc2b601cd006240e8
vbvsingh29 <vbvsingh2905@gmail.com>
2024-03-07 20:37:50 +0530

    added CORS

--- src/utlis/server.ts
+++ src/utlis/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import config from "config";
+import cors from "cors";
 import deserializeUser from "../middleware/deserializeUser";
 import routes from "../routes";
 function createServer() {
   const app = express();
-
+  app.use(
+    cors({
+      origin: config.get("origin"),
+      credentials: true,
+    })
+  );
   app.use(express.json());
   app.use(deserializeUser);
   routes(app);
